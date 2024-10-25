a663660a9d8f149ae299b3624609d004c4f13667
NAlexsandr <132039918+NAlexsandr@users.noreply.github.com>
2024-10-25 19:13:17 +0700

    Lesson11

---
+++ data.js
@@ -0,0 +1,44 @@
+const dataProducts =`[
+  {
+      "url": "./img/card2/Foto1.jpg",
+      "title": "LLERY X M'O CAPSULE",
+      "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+      "price": "$52.00"
+  },
+
+  {
+      "url": "./img/card2/Foto2.png",
+      "title": "LLERY X M'O CAPSULE",
+      "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+      "price": "$52.00"
+  },
+
+  {
+      "url": "./img/card2/Foto3.png",
+      "title": "LLERY X M'O CAPSULE",
+      "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+      "price": "$52.00"
+  },
+
+  {
+      "url": "./img/card2/Foto4.png",
+      "title": "LLERY X M'O CAPSULE",
+      "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+      "price": "$52.00"
+  },
+
+  {
+      "url": "./img/card2/Foto5.png",
+      "title": "LLERY X M'O CAPSULE",
+      "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+      "price": "$52.00"
+  },
+
+  {
+      "url": "./img/card2/Foto6.png",
+      "title": "LLERY X M'O CAPSULE",
+      "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+      "price": "$52.00"
+  }
+
+]`
--- script.js
+++
@@ -1,7 +0,0 @@
-const menuActive = document.querySelector('.menu-active');
-const burger = document.querySelector('.burger');
-
-function toggleMenu(){
-  menuActive.classList.toggle('hidden')
-}
-burger.addEventListener('click', toggleMenu);
